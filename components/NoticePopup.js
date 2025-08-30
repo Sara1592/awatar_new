8c44ce9bec602d8099d96727d57953448c8fc33a
Sara <117970577+Sara1592@users.noreply.github.com>
2025-08-30 19:53:04 +0530

    popup_button change

--- components/NoticePopup.js
+++ components/NoticePopup.js
@@ -1,62 +1,7 @@
-
-// "use client";
-// import { useEffect, useState } from "react";
-// import { useRouter } from "next/router";
-
-// export default function NoticePopup() {
-//   const [open, setOpen] = useState(false);
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     if (router.pathname === "/") {
-//       const timer = setTimeout(() => setOpen(true), 400); // open after 400ms
-//       return () => clearTimeout(timer);
-//     }
-//   }, [router.pathname]);
-
-//   if (!open) return null;
-
-//   return (
-//     <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
-//       <div className="bg-black w-11/12 max-w-md rounded-2xl p-6 shadow-xl relative text-center animate-scaleIn">
-        
-//         {/* Close Button */}
-//         <button
-//   onClick={() => setOpen(false)}
-//   className="absolute top-3 left-30 text-gray-600 hover:text-black text-2xl"
-// >
-//   ×
-// </button>
-
-//         {/* Title */}
-//         <h2 className="text-xl md:text-2xl font-bold text-gray-800 mb-3">
-//           Important Notice
-//         </h2>
-
-//         {/* Text */}
-//         <p className="text-sm md:text-base text-white-700 leading-relaxed mb-5">
-//           We do not allow children and do not <br />
-//           offer a non-smoking section.
-//         </p>
-
-//         {/* CTA */}
-//         <a
-//           href="/"
-//           className="inline-block px-5 py-2.5 bg-yellow-500 hover:bg-yellow-600 
-//                      text-black font-semibold rounded-xl transition"
-//         >
-//           Get In Touch
-//         </a>
-//       </div>
-//     </div>
-//   );
-// }
-
-
-
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function NoticePopup() {
   const [open, setOpen] = useState(false);
@@ -73,23 +18,22 @@ export default function NoticePopup() {
 
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
+      
       <div className="bg-black w-11/12 max-w-md rounded-2xl p-6 shadow-xl relative text-center animate-scaleIn">
         
-        {/* Close Button */}
-        {/* <button
-          onClick={() => setOpen(false)}
-          className="absolute top-3 left-3 text-gray-400 hover:text-white text-lg md:text-xl"
-        >
-          ×
-        </button> */}
-
-        <button
+<div
+  className="absolute top-3 right-3 w-8 h-8 flex items-center justify-center
+             bg-[#8E6E3B] hover:bg-[#8E6E3B] text-white text-sm rounded-full cursor-pointer"
   onClick={() => setOpen(false)}
-  className="absolute top-3 left-30 w-8 h-8 flex 
-             bg-gray-700 hover:bg-gray-600 text-white text-sm rounded-full"
 >
   ×
-</button>
+</div>
+{/* 
+        <button
+  onClick={() => setOpen(false)}
+  className="close-btn">
+  ×
+</button> */}
 
         {/* Title */}
         <h2 className="text-xl md:text-2xl font-bold text-gray-100 mb-3">
@@ -103,13 +47,12 @@ export default function NoticePopup() {
         </p>
 
         {/* CTA */}
-        <a
-          href="/"
-          className="inline-block px-5 py-2.5 bg-yellow-500 hover:bg-yellow-600 
-                     text-black font-semibold rounded-xl transition"
-        >
-          Get In Touch
-        </a>
+        <Link
+          href="contact"
+           onClick={() => setOpen(false)}
+          className="kf-btn">
+         <span>Get In Touch</span> 
+        </Link>
       </div>
     </div>
   );
