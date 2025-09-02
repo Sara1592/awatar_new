6b287de360934ec2210625371529aaadfbf9a523
Sara <117970577+Sara1592@users.noreply.github.com>
2025-09-02 13:01:20 +0530

    rem_sitemap

--- next-sitemap.config.js
+++
@@ -1,4 +0,0 @@
- module.exports = {
-      siteUrl: process.env.SITE_URL || 'https://awtarcafe.com/', // Replace with your actual site URL
-      generateRobotsTxt: true, // Optional: Generates a robots.txt file as well
-    };
